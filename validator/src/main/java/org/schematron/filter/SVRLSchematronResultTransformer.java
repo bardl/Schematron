dc4f19fa1b10e90145b67674d69e7952eb7120f7
Bård Langöy <bard.langoy@gmail.com>
2013-12-06 17:07:43 +0100

    Fixed possible dirty SchematronResultTransformer when resued in several transformations

--- validator/src/main/java/org/schematron/filter/SVRLSchematronResultTransformer.java
+++ validator/src/main/java/org/schematron/filter/SVRLSchematronResultTransformer.java
@@ -20,84 +20,96 @@ import java.io.OutputStreamWriter;
  * {@link org.schematron.validation.SchematronValidator}.
  * @author bard.langoy
  */
-public class SVRLSchematronResultTransformer extends XMLFilterImpl implements SchematronResultTransformer {
-
-    protected ElementPath currentElement;
-    protected boolean valid = true;
-    protected SchematronResultImpl result;
-
-    public SVRLSchematronResultTransformer() throws SAXException {
-        super();
-        setParent(XMLReaderFactory.createXMLReader());
-        setContentHandler(new XMLWriter(new OutputStreamWriter(new ByteArrayOutputStream())));
-    }
+public class SVRLSchematronResultTransformer implements SchematronResultTransformer {
 
     public SchematronResult transform(InputSource source) throws SchematronException {
-        result = new SchematronResultImpl();
         try {
-            parse(source);
-            return result;
-        } catch (Exception e) {
+            return new SVRLXMLFilter().transform(source);
+        } catch (SAXException e) {
             throw new SchematronException("Error occured when transforming Schematron Validation Report Language (SVRL) to SchematronResult.", e);
         }
     }
 
-    public void startElement(String namespaceURI, String localName, String qualifiedName, Attributes atts) throws SAXException {
-        currentElement = new ElementPath(localName, currentElement);
-        if (currentElement.name.equals("failed-assert")) {
-            currentElement.setMessage(atts.getValue("test"));
-            currentElement.setLevel(convertFlagToValidationLevel(atts.getValue("flag")));
+    private class SVRLXMLFilter extends XMLFilterImpl {
+        protected ElementPath currentElement;
+        protected boolean valid = true;
+        protected SchematronResultImpl result;
 
-            if (currentElement.getLevel() == ValidationLevel.ERROR || currentElement.getLevel() == ValidationLevel.FATAL) {
-                valid = false;
+        private SVRLXMLFilter() throws SAXException {
+            super();
+            setParent(XMLReaderFactory.createXMLReader());
+            setContentHandler(new XMLWriter(new OutputStreamWriter(new ByteArrayOutputStream())));
+        }
+
+        public SchematronResult transform(InputSource source) throws SchematronException {
+            result = new SchematronResultImpl();
+            currentElement = null;
+            valid = true;
+            try {
+                parse(source);
+                return result;
+            } catch (Exception e) {
+                throw new SchematronException("Error occured when transforming Schematron Validation Report Language (SVRL) to SchematronResult.", e);
             }
         }
 
-        super.startElement(namespaceURI, localName, qualifiedName, atts);
-    }
+        public void startElement(String namespaceURI, String localName, String qualifiedName, Attributes atts) throws SAXException {
+            currentElement = new ElementPath(localName, currentElement);
+            if (currentElement.name.equals("failed-assert")) {
+                currentElement.setMessage(atts.getValue("test"));
+                currentElement.setLevel(convertFlagToValidationLevel(atts.getValue("flag")));
 
-    private ValidationLevel convertFlagToValidationLevel(String value) {
-        ValidationLevel result = null;
-        if ("warning".equals(value.toLowerCase())) {
-            result = ValidationLevel.WARN;
-        } else if ("error".equals(value.toLowerCase())) {
-            result = ValidationLevel.ERROR;
-        } else if ("fatal".equals(value.toLowerCase())) {
-            result = ValidationLevel.FATAL;
-        }
-
-        if (result == null) {
-            throw new SchematronException("Unable to parse validation error level [" + value + "] when executing validation filter for test [" + currentElement.getMessage() + "].");
-        }
-
-        return result;
-    }
-
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        if (currentElement.name.equals("text")) {
-            StringBuilder message = new StringBuilder();
-            message.append(ch, start, length);
-            message.append(" ");
-            message.append(currentElement.parent.getLevel().getDisplayString());
-            message.append(" when performing test [");
-            message.append(currentElement.parent.getMessage());
-            message.append("].");
-
-            if (currentElement.parent.getLevel() == ValidationLevel.ERROR) {
-                result.getErrors().add(new Assertion(currentElement.parent.name, message.toString()));
-            } else if (currentElement.parent.getLevel() == ValidationLevel.WARN) {
-                result.getWarnings().add(new Assertion(currentElement.parent.name, message.toString()));
-            } else if (currentElement.parent.getLevel() == ValidationLevel.FATAL) {
-                result.getFatals().add(new Assertion(currentElement.parent.name, message.toString()));
+                if (currentElement.getLevel() == ValidationLevel.ERROR || currentElement.getLevel() == ValidationLevel.FATAL) {
+                    valid = false;
+                }
             }
+
+            super.startElement(namespaceURI, localName, qualifiedName, atts);
         }
 
-        super.characters(ch, start, length);
-    }
+        private ValidationLevel convertFlagToValidationLevel(String value) {
+            ValidationLevel result = null;
+            if ("warning".equals(value.toLowerCase())) {
+                result = ValidationLevel.WARN;
+            } else if ("error".equals(value.toLowerCase())) {
+                result = ValidationLevel.ERROR;
+            } else if ("fatal".equals(value.toLowerCase())) {
+                result = ValidationLevel.FATAL;
+            }
 
-    public void endElement(String namespaceURI, String localName, String qualifiedName) throws SAXException {
-        currentElement = currentElement.parent;
-        super.endElement(namespaceURI, localName, qualifiedName);
+            if (result == null) {
+                throw new SchematronException("Unable to parse validation error level [" + value + "] when executing validation filter for test [" + currentElement.getMessage() + "].");
+            }
+
+            return result;
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            if (currentElement.name.equals("text")) {
+                StringBuilder message = new StringBuilder();
+                message.append(ch, start, length);
+                message.append(" ");
+                message.append(currentElement.parent.getLevel().getDisplayString());
+                message.append(" when performing test [");
+                message.append(currentElement.parent.getMessage());
+                message.append("].");
+
+                if (currentElement.parent.getLevel() == ValidationLevel.ERROR) {
+                    result.getErrors().add(new Assertion(currentElement.parent.name, message.toString()));
+                } else if (currentElement.parent.getLevel() == ValidationLevel.WARN) {
+                    result.getWarnings().add(new Assertion(currentElement.parent.name, message.toString()));
+                } else if (currentElement.parent.getLevel() == ValidationLevel.FATAL) {
+                    result.getFatals().add(new Assertion(currentElement.parent.name, message.toString()));
+                }
+            }
+
+            super.characters(ch, start, length);
+        }
+
+        public void endElement(String namespaceURI, String localName, String qualifiedName) throws SAXException {
+            currentElement = currentElement.parent;
+            super.endElement(namespaceURI, localName, qualifiedName);
+        }
     }
 }
--- validator/src/test/java/org/schematron/filter/OIOUBLSchematronResultTransformer.java
+++ validator/src/test/java/org/schematron/filter/OIOUBLSchematronResultTransformer.java
@@ -20,64 +20,77 @@ import java.io.OutputStreamWriter;
  * {@link org.schematron.validation.SchematronValidator}.
  * @author bard.langoy
  */
-public class OIOUBLSchematronResultTransformer extends XMLFilterImpl implements SchematronResultTransformer {
-
-    protected ElementPath currentElement;
-    protected boolean valid = true;
-    protected SchematronResultImpl result = new SchematronResultImpl();
-
-    private final String ERROR_ELEMENT = "Error";
-    private final String DESCRIPTION_ELEMENT = "Description";
-    private final String CONDITION_ELEMENT = "Pattern";
-
-    public OIOUBLSchematronResultTransformer() throws SAXException {
-        super();
-        setParent(XMLReaderFactory.createXMLReader());
-        setContentHandler(new XMLWriter(new OutputStreamWriter(new ByteArrayOutputStream())));
-    }
+public class OIOUBLSchematronResultTransformer implements SchematronResultTransformer {
 
     public SchematronResult transform(InputSource source) throws SchematronException {
-        result = new SchematronResultImpl();
         try {
-            parse(source);
-            return result;
-        } catch (Exception e) {
+            return new OIOUBLXMLFilter().transform(source);
+        } catch (SAXException e) {
             throw new SchematronException("Error occured when transforming OIOUBL validation report to SchematronResult.", e);
         }
     }
 
-    public void startElement(String namespaceURI, String localName, String qualifiedName, Attributes atts) throws SAXException {
-        currentElement = new ElementPath(localName, currentElement);
-        if (currentElement.name.equals(ERROR_ELEMENT)) {
-            valid = false;
-            currentElement.setMessage(atts.getValue("context"));
+    private class OIOUBLXMLFilter extends XMLFilterImpl {
+
+        protected ElementPath currentElement;
+        protected boolean valid = true;
+        protected SchematronResultImpl result;
+
+        private final String ERROR_ELEMENT = "Error";
+        private final String DESCRIPTION_ELEMENT = "Description";
+        private final String CONDITION_ELEMENT = "Pattern";
+
+        private OIOUBLXMLFilter() throws SAXException {
+            super();
+            setParent(XMLReaderFactory.createXMLReader());
+            setContentHandler(new XMLWriter(new OutputStreamWriter(new ByteArrayOutputStream())));
         }
 
-        super.startElement(namespaceURI, localName, qualifiedName, atts);
-    }    
-
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        if (currentElement.name.equals(CONDITION_ELEMENT)) {
-            currentElement.setMessage(new String(ch, start, length));
-        } else if (currentElement.name.equals(DESCRIPTION_ELEMENT)) {
-            currentElement.setMessage(new String(ch, start, length));
+        public SchematronResult transform(InputSource source) throws SchematronException {
+            result = new SchematronResultImpl();
+            currentElement = null;
+            valid = true;
+            try {
+                parse(source);
+                return result;
+            } catch (Exception e) {
+                throw new SchematronException("Error occured when transforming OIOUBL validation report to SchematronResult.", e);
+            }
         }
 
-        super.characters(ch, start, length);
-    }
+        public void startElement(String namespaceURI, String localName, String qualifiedName, Attributes atts) throws SAXException {
+            currentElement = new ElementPath(localName, currentElement);
+            if (currentElement.name.equals(ERROR_ELEMENT)) {
+                valid = false;
+                currentElement.setMessage(atts.getValue("context"));
+            }
 
-    public void endElement(String namespaceURI, String localName, String qualifiedName) throws SAXException {
-        if (localName.equals(ERROR_ELEMENT)) {            
-            StringBuilder message = new StringBuilder();
-            message.append(currentElement.getChildElementWithName(DESCRIPTION_ELEMENT).getMessage());
-            message.append(", failed when performing test [");
-            message.append(currentElement.getChildElementWithName(CONDITION_ELEMENT).getMessage());
-            message.append("] in context [").append(currentElement.getMessage()).append("].");
-
-            result.getErrors().add(new Assertion(currentElement.name, message.toString()));
+            super.startElement(namespaceURI, localName, qualifiedName, atts);
         }
-        currentElement = currentElement.parent;
-        super.endElement(namespaceURI, localName, qualifiedName);
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            if (currentElement.name.equals(CONDITION_ELEMENT)) {
+                currentElement.setMessage(new String(ch, start, length));
+            } else if (currentElement.name.equals(DESCRIPTION_ELEMENT)) {
+                currentElement.setMessage(new String(ch, start, length));
+            }
+
+            super.characters(ch, start, length);
+        }
+
+        public void endElement(String namespaceURI, String localName, String qualifiedName) throws SAXException {
+            if (localName.equals(ERROR_ELEMENT)) {
+                StringBuilder message = new StringBuilder();
+                message.append(currentElement.getChildElementWithName(DESCRIPTION_ELEMENT).getMessage());
+                message.append(", failed when performing test [");
+                message.append(currentElement.getChildElementWithName(CONDITION_ELEMENT).getMessage());
+                message.append("] in context [").append(currentElement.getMessage()).append("].");
+
+                result.getErrors().add(new Assertion(currentElement.name, message.toString()));
+            }
+            currentElement = currentElement.parent;
+            super.endElement(namespaceURI, localName, qualifiedName);
+        }
     }
 }
